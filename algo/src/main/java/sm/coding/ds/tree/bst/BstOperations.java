2574b9861696796948f42a213c7a95157f37de59
shahzadmughal8410 <shahzad.mughal8410@gmail.com>
2018-11-08 08:33:19 -0800

    delete implememted

--- algo/src/main/java/sm/coding/ds/tree/bst/BstOperations.java
+++ algo/src/main/java/sm/coding/ds/tree/bst/BstOperations.java
@@ -33,6 +33,51 @@ public class BstOperations {
 		
 		return n;
 	}
+
+	public static TreeNode deleteNode(TreeNode root, int key) {
+	    if(root == null){
+	        return null;
+	    }
+	    if(key < root.val){
+	        root.left = deleteNode(root.left, key);
+	    }else if(key > root.val){
+	        root.right = deleteNode(root.right, key);
+	    }else{
+	        if(root.left == null){
+	            return root.right;
+	        }else if(root.right == null){
+	            return root.left;
+	        }
+	        
+	        TreeNode minNode = findMin(root.right);
+	        root.val = minNode.val;
+	        root.right = deleteNode(root.right, root.val);
+	    }
+	    return root;
+	}
+
+	/**
+Steps:
+
+    Recursively find the node that has the same value as the key, while setting the left/right nodes equal to the returned subtree
+    Once the node is found, have to handle the below 4 cases
+
+    node doesn't have left or right - return null
+    node only has left subtree- return the left subtree
+    node only has right subtree- return the right subtree
+    node has both left and right - find the minimum value in the right subtree, set that value to the currently found node, then recursively delete the minimum value in the right subtree
+
+https://leetcode.com/problems/delete-node-in-a-bst/discuss/93296/Recursive-Easy-to-Understand-Java-Solution
+	 * @param node
+	 * @return
+	 */
+	private static TreeNode findMin(TreeNode node){
+	    while(node.left != null){
+	        node = node.left;
+	    }
+	    return node;
+	}
+
 	
 	public static void main(String[] args) {
 		testInsertInExistingTree();
