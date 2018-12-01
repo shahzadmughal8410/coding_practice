e99b241b766fb107700b9015f6d80a6d3cd79f4b
shahzadmughal8410 <shahzad.mughal8410@gmail.com>
2018-12-01 14:02:37 -0800

    updated with minor improvements

--- algo/src/main/java/sm/interview/coding_challange/company_10/TwoSum_ThreeSum_Sum_Zero.java
+++ algo/src/main/java/sm/interview/coding_challange/company_10/TwoSum_ThreeSum_Sum_Zero.java
@@ -12,84 +12,79 @@ import java.util.Set;
  */
 public class TwoSum_ThreeSum_Sum_Zero {
 
-	
 	/*
-	 * Given an array of integers, how would you determine if it contains a pair of numbers that add up to 0?
-	Examples: [3, -2, 1, 7, -3, 5, 9] -> true
-	          [3, -2, -1, 7, 3, 5, 9] -> false
-	          [3, -2, 1, 7, -4, 5, 9] -> false
-	 
-	 T = O (n2)
-	 S = O(1)
-
-	 T = O (n)
-	 S = O(n)
-
-	HashSet 
-	0-3 = -3 
-	0-2 = 2 
-
-	-----------
-
-	 T = O (n2), 
-	 S = O(n)
-
-
-	[3, -2, -1, 7, 3, 5, 9]
-
-	[1,5,3]
-
-	0-3 = -3
-
+	 * Given an array of integers, how would you determine if it contains a pair of
+	 * numbers that add up to 0? Examples: [3, -2, 1, 7, -3, 5, 9] -> true [3, -2,
+	 * -1, 7, 3, 5, 9] -> false [3, -2, 1, 7, -4, 5, 9] -> false
+	 * 
+	 * T = O (n2) S = O(1)
+	 * 
+	 * T = O (n) S = O(n)
+	 * 
+	 * HashSet 0-3 = -3 0-2 = 2
+	 * 
+	 * -----------
+	 * 
+	 * T = O (n2), S = O(n)
+	 * 
+	 * 
+	 * [3, -2, -1, 7, 3, 5, 9]
+	 * 
+	 * [1,5,3]
+	 * 
+	 * 0-3 = -3
+	 * 
 	 */
-	  
-	  public static boolean findTriplets(int[] nums){
-	      if(null==nums || nums.length<3){
-	        return false;
-	      }
-	    
-	    for(int i=0;i<nums.length;i++){
-	        int sum = 0-nums[i];
-	        if(findPair(nums, sum, i)){
-	          return true;
-	        }
-	      }
-	    
-	    return false;
 
-	  }
-	  
-	  public static boolean findPair(int[] nums, int sum, int choosen){
-	    Set<Integer> set = new HashSet<>();
-	    
-	    for(int i=0;i<nums.length;i++){
-	      if(i==choosen){
-	        continue;
-	      }
-	      if(set.contains(nums[i])){
-	        return true;
-	      }else{
-	        set.add(sum-nums[i]);
-	      }
-	    }
-	    
-	    return false;
-	  }
-	
+	public static boolean findTriplets(int[] nums) {
+		if (null == nums || nums.length < 3) {
+			return false;
+		}
+
+		for (int i = 0; i < nums.length - 2; i++) { // we can run loop to length-2, as if we reached to length-3 means
+													// no need to check last 2 elements
+			int sum = 0 - nums[i];
+			if (findPair(nums, sum, i)) {
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+
+	public static boolean findPair(int[] nums, int sum, int choosen) {
+		if (null == nums || nums.length < 2) {
+			return false;
+		}
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (i == choosen) { // skip the selected number from findTriplet
+				continue;
+			}
+			if (set.contains(nums[i])) {
+				return true;
+			} else {
+				set.add(sum - nums[i]);
+			}
+		}
+
+		return false;
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	    int[] nums = new int[]{3, -2, -1, 7, 3, 5, 9};
-	    boolean zeroSum = findTriplets(nums);
-	    System.out.println("zeroSum="+zeroSum);
+		int[] nums = new int[] { 3, -2, -1, 7, 3, 5, 9 };
+		boolean zeroSum = findTriplets(nums);
+		System.out.println("zeroSum=" + zeroSum);
 
-	    nums = new int[]{1, 5, 3};
-	    zeroSum = findTriplets(nums);
-	    System.out.println("zeroSum="+zeroSum);
+		nums = new int[] { -1, -2, 3 };
+		zeroSum = findTriplets(nums);
+		System.out.println("zeroSum=" + zeroSum);
 
 	}
 
 }
-
-
