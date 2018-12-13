6739bd141797ee7b3276718628303c3fdaaa5882
shahzadmughal8410 <shahzad.mughal8410@gmail.com>
2018-12-13 00:37:27 -0800

    solution updated to match the minimum window and at most k distinct elements

--- algo/src/main/java/sm/coding/string/leetcode/_003/LongestSubstringWithoutRepeatingCharacters_003_Str_2P.java
+++ algo/src/main/java/sm/coding/string/leetcode/_003/LongestSubstringWithoutRepeatingCharacters_003_Str_2P.java
@@ -28,9 +28,9 @@ https://leetcode.com/problems/longest-substring-without-repeating-characters/sol
 https://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
 
 Submission
-https://leetcode.com/submissions/detail/189478610/
+https://leetcode.com/submissions/detail/194882752/
 You are here! 
-Your runtime beats 61.84 % of java submissions.
+Your runtime beats 92.26 % of java submissions.
 	 * 
 	 * @param args
 	 */
@@ -39,22 +39,32 @@ Your runtime beats 61.84 % of java submissions.
 			return 0;
 		}
 		int maxLength = 0;
+		int start = 0;
+		int startIndex = -1;
 		Map<Character, Integer> map = new HashMap<>();
-		for(int start = 0, end =0; end<s.length(); end++) {
-			if(map.containsKey(s.charAt(end))) {
-				start = Math.max(map.get(s.charAt(end)), start);
+		for(int end =0; end<s.length(); end++) {
+			char c = s.charAt(end);
+			if(map.containsKey(c)) {
+				start = Math.max(map.get(c), start);
 			}
-			maxLength = Math.max(maxLength, end-start+1);
-			map.put(s.charAt(end),end+1);
+
+			map.put(c,end+1);
+			
+			int currentLength = end-start+1;
+			if(maxLength < currentLength) {
+				maxLength = currentLength;
+				startIndex = start;
+			}
 		}
+		System.out.println(s.substring(startIndex, startIndex+maxLength));
 		return maxLength;
 	}
 	
 	
 	public static void main(String[] args) {
 		String s = "abcabcbb";
-//		System.out.println("Max length non repeating characters="+findLengthLongestSubstringNoRepeatingCharacters(s));
-		System.out.println("Max length non repeating characters="+SolutionDebug.findLengthLongestSubstringNoRepeatingCharacters(s));
+		System.out.println("Max length non repeating characters="+lengthOfLongestSubstring(s));
+//		System.out.println("Max length non repeating characters="+SolutionDebug.findLengthLongestSubstringNoRepeatingCharacters(s));
 
 	}
 
