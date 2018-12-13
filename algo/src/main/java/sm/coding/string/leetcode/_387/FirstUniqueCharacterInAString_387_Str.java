1bc044376069657d781b63b08437b6499b326f9e
shahzadmughal8410 <shahzad.mughal8410@gmail.com>
2018-12-12 23:21:18 -0800

    code cleaned up submission added

--- algo/src/main/java/sm/coding/string/leetcode/_387/FirstUniqueCharacterInAString_387_Str.java
+++ algo/src/main/java/sm/coding/string/leetcode/_387/FirstUniqueCharacterInAString_387_Str.java
@@ -26,38 +26,28 @@ Note: You may assume the string contain only lowercase letters.
 
 https://leetcode.com/problems/first-unique-character-in-a-string/description/
 https://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character/
- 
+
+Submission
+https://leetcode.com/submissions/detail/194871465/
+You are here! 
+Your runtime beats 34.41 % of java submissions. 
 	 * @param args
 	 */
-	public static int firstUniqueCharacterIndex(String s) {
+	public static int firstUniqChar(String s) {
 		if(null==s || s.length()==0) {
 			return -1;
 		}
-		Map<Character, FirstIndex> m = new LinkedHashMap<>();
+		Map<Character, Integer> m = new LinkedHashMap<>();
 		for(int i = 0; i<s.length();i++) {
-			char key = s.charAt(i);
-			FirstIndex f = m.computeIfAbsent(key, K -> {return new FirstIndex();}  );
-			/* above line is equal to following code
-			FirstIndex f = null;
-			if(m.containsKey(key)) {
-				f = m.get(key);
-			}else {
-				f = new FirstIndex();
-			}
-			*/
-			
-			f.count++;
-			if(f.firstIndex==0) {
-				f.firstIndex = i;
-			}
-			m.put(key,f);
+			char c = s.charAt(i);
+			m.put(c, m.getOrDefault(c, 0)+1);
 		}
 		// loop through the keys
 		// keys are in insertion order in linked hashmap so the first character with count 1 
 		// in first chartacter in string as well
-		for(Map.Entry<Character, FirstIndex> entry : m.entrySet()) {
-			if(entry.getValue().count==1) {
-				return entry.getValue().firstIndex; 
+		for(int i=0; i<s.length();i++) {
+			if(m.get(s.charAt(i))==1) {
+				return i; 
 			}
 		}
 		return -1;
@@ -65,14 +55,8 @@ https://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-charac
 	
 	public static void main(String[] args) {
 		String s = "loveleetcode";
-		System.out.println("Firest unique charecter index is="+firstUniqueCharacterIndex(s));
+		System.out.println("Firest unique charecter index is="+firstUniqChar(s));
 
 	}
 
-}
-
-
-class FirstIndex {
-	int count; 
-	int firstIndex;
 }
