ad4333692ba95d56d44ab5b47250a6bad2899eed
shahzadmughal8410 <shahzad.mughal8410@gmail.com>
2018-12-12 23:30:26 -0800

    simplified implementation

--- algo/src/main/java/sm/coding/string/leetcode/_344/ReverseString_344_Str_2P.java
+++ algo/src/main/java/sm/coding/string/leetcode/_344/ReverseString_344_Str_2P.java
@@ -32,29 +32,43 @@ https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
 	 * 
 	 * T=O(n)
 	 * S=O(n)
-	 * 
+
+Submission
+https://leetcode.com/submissions/detail/194873231/
+You are here! 
+Your runtime beats 100.00 % of java submissions.
 	 * @param args
 	 */
-	public static String reverse(String input) {
-		if(null==input || input.length()==0) {
-			return input;
+	public static String reverseString(String s) {
+		if(null==s || s.length()==0) {
+			return s;
 		}		
-		char[] arr = input.toCharArray();
-		int mid = input.length()/2;
-		for(int i =0, j=input.length()-1;i<mid;i++,j--) {
-			char tmp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = tmp;
+		char[] arr = s.toCharArray();
+		int start = 0;
+		int end = s.length()-1;
+		
+		while(start<end) {
+			swap(arr, start++,end--);
 		}		
 		return new String(arr);		
 	}
 	
+	public static void swap(char[] arr, int a, int b) {
+		char tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
 	
 	public static void main(String[] args) {
 		String input = "12345";
-		String reverse = reverse(input);
+		String reverse = reverseString(input);
 		System.out.println(input);
 		System.out.println(reverse);
-	}
+
+		input = "abcd";
+		reverse = reverseString(input);
+		System.out.println(input);
+		System.out.println(reverse);
+}
 
 }
