672923d5983b46fde58f18db336b61eb62c89264
shahzadmughal8410 <shahzad.mughal8410@gmail.com>
2019-01-02 15:09:41 -0800

    solution impproved

--- algo/src/main/java/sm/coding/ds/llstq/leetcode/stack/_227/BasicCalculatorII_227.java
+++ algo/src/main/java/sm/coding/ds/llstq/leetcode/stack/_227/BasicCalculatorII_227.java
@@ -80,25 +80,81 @@ Your runtime beats 83.75 % of java submissions.
 		return result;
     }
 	
+    /**
+Submission
+https://leetcode.com/submissions/detail/198595129/
+You are here! 
+Your runtime beats 78.77 % of java submissions.
+     * @param s
+     * @return
+     */
+    public static int calculate_NoStack(String s) {
+        if(null==s || s.length()==0) {
+        		return 0;
+        }
+        int num = 0;
+        int previous = 0;
+        int result = 0;
+        char op =  '+'; // by default set operator to plus
+        for(int i =0; i<s.length();i++) {
+        		char c = s.charAt(i);
+        		// to parse consecutive digits
+        		if(Character.isDigit(c)) {
+        			num = num * 10 + (c-'0');
+        		}
+        		
+        		if( (!Character.isDigit(c) && ' '!=c ) || i == s.length()-1) {
+        			// process previous op with last num, and update this operator i.e. op=c
+        			switch(op) {
+	    				case '+':
+	    					result +=previous;
+	    					previous=num;
+	    					break;
+	    				case '-':
+	    					result+=previous;
+	    					previous = -num;
+	    					break;
+	    				case '*':
+	    					previous = previous * num;
+	    					break;
+	    				case '/':
+	    					previous = previous/num;
+	    					break;
+        			}        			 
+				op = c;  // set operator 
+				num = 0; // reset num to 0
+        		}        		
+        }
+        result+=previous;// add the last number stored as previous
+		return result;
+    }
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		String s = "5+2*3";
 		int result = calculate(s);
-		System.out.println(s+" -> "+result);
+		System.out.println("Stack    = "+s+" -> "+result);
+		result = calculate_NoStack(s);
+		System.out.println("No Stack = "+s+" -> "+result);
 		
 		s = "3+2*2";
 		result = calculate(s);
-		System.out.println(s+" -> "+result);
+		System.out.println("Stack    = "+s+" -> "+result);
+		result = calculate_NoStack(s);
+		System.out.println("No Stack = "+s+" -> "+result);
 		
 		s = "1 0 0 * 1 0 ";
 		result = calculate(s);
-		System.out.println(s+" -> "+result);
+		System.out.println("Stack    = "+s+" -> "+result);
+		result = calculate_NoStack(s);
+		System.out.println("No Stack = "+s+" -> "+result);
 		
 		s = "1 0 0 + 1 0 ";
 		result = calculate(s);
-		System.out.println(s+" -> "+result);
+		System.out.println("Stack    = "+s+" -> "+result);
+		result = calculate_NoStack(s);
+		System.out.println("No Stack = "+s+" -> "+result);
 		
 	}
 
